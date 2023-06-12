d381b0417a4f114942c40a1bb52bfda8fbe80df5
Karanveer2202 <sirohikaranveer@gmail.com>
2023-06-12 22:31:43 +0530

    change image

--- src/components/Section.js
+++ src/components/Section.js
@@ -25,7 +25,7 @@ function Section({title, description, rightBtnText,leftBtnText, backgroundImg })
           </ButtonGroup>
         </Fade>
         
-        <DownArrow src="/images/down-arrow.svg"/>
+        <DownArrow src="images/down-arrow.svg"/>
       </Buttons>
       
     </Wrap>
@@ -39,12 +39,12 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url('/images/model-s.jpg');
+  background-image: url('images/model-s.jpg');
   display: flex;
   flex-direction: column;
   justify-content: space-between; //vertical alignment
   align-items: center; //horizontal alignment
-  background-image: ${props => `url("/images/${props.bgImage}")`}
+  background-image: ${props => `url("images/${props.bgImage}")`}
 `
 
 const ItemText = styled.div`
